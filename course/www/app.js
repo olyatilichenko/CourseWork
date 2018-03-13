3d02bc887cda258d21ef4a9b9d9271e9251e28b5
Olya <olyat96@gmail.com>
2018-03-14 01:35:14 +0300

    first commit

---
+++ course/www/app.js
@@ -0,0 +1,57 @@
+
+// Создаем новый элемент нажатием кнопки "Add"
+function newElement() {
+
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        document.getElementById("myList").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
+// Создаем кнопку "close" для каждого элемента
+var myNodelist = document.getElementsByTagName("li");
+var i;
+for ( i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+
+// Нажимаем на кнопку для закрытия текущего элемента
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+// Создаем символ зачеркивания
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
